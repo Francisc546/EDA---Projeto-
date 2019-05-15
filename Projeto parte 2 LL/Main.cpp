ef65799c96dfa6b91620718f8f98a6268abcf160
pedrotiago12345 <pedrotiagocg2011@hotmail.com>
2019-05-15 16:59:49 +0100

    mudei o main na parte do gravar

--- Projeto parte 2 LL/Main.cpp
+++ Projeto parte 2 LL/Main.cpp
@@ -9,52 +9,53 @@ using namespace std;
 
 
 int main() {
+	system("color 0A");
 	bool running = true;
 	srand(time(NULL));
 	int tamanhopNome = contaLinhas("primeiro_nome.txt");
 	int tamanhouNome = contaLinhas("ultimo_nome.txt");
 	int tamanhocursos = contaLinhas("cursos.txt");
-	string * pNome = new string[tamanhopNome];
-	string * uNome = new string[tamanhouNome];
-	string * cursos = new string[tamanhocursos];
-	Insere(pNome, "primeiro_nome.txt",tamanhopNome); // insere os primeiros nomes no array pNome
-	Insere(uNome, "ultimo_nome.txt",tamanhouNome); // insere os ultimos nomes no array uNome
-	Insere(cursos, "cursos.txt",tamanhocursos); // insere os cursos no array cursos
+	string* pNome = new string[tamanhopNome];
+	string* uNome = new string[tamanhouNome];
+	string* cursos = new string[tamanhocursos];
+	Insere(pNome, "primeiro_nome.txt", tamanhopNome); // insere os primeiros nomes no array pNome
+	Insere(uNome, "ultimo_nome.txt", tamanhouNome); // insere os ultimos nomes no array uNome
+	Insere(cursos, "cursos.txt", tamanhocursos); // insere os cursos no array cursos
 
-	identidade * filadeespera = NULL;
+	identidade* filadeespera = NULL;
 	filadeespera = criagrupo(filadeespera, pNome, uNome, cursos);
-	
 
-	Mesa*mesas = NULL;
+
+	Mesa* mesas = NULL;
 	int tamanhodacantina = rand() % 20 + 30;
 	mesas = criamesas(mesas, tamanhodacantina);
-	
-	
+
+
 
 
 
 	do {
 		int ciclos = 10;
-		refeicao * novaref = new refeicao;
+		refeicao* novaref = new refeicao;
 		InsereRefeicao(novaref);
 		system("cls");
 		cout << "-------------------------CANTINA EDA ------------------------------" << endl;
 		ImprimeRefeicao(novaref);
+		cout << "----------------------------------" << endl;
 		escreveFiladeEspera(filadeespera);
 		imprimeCantina(mesas);
 
 		while (ciclos != 0) {
-
-			cout << "(s) seguinte (e) Emergência (g) Gravar (c) Carregar dados (o) Opçoes ";
+			cout << endl;
+			cout << "----------------------------------------------------------------------" << endl;
+			cout << " (s) seguinte (e) Emergência (g) Gravar (c) Carregar dados (o) Opçoes ";
 			char opcao;
 			cin >> opcao;
 			switch (opcao) {
-			case 's':
+			case 's':{
 				int special = rand() % 100;
 				if (special >= 95) {
 					filadeespera = criaespecial(filadeespera, pNome, uNome, cursos);
-
-
 				}
 				else {
 					filadeespera = criagrupo(filadeespera, pNome, uNome, cursos);
@@ -63,14 +64,50 @@ int main() {
 				imprimeCantina(mesas);
 				ciclos--;
 				break;
-			/*case 'e':
-				break;
-			case 'g':
-				break;
-			case 'c':
-				break;
-			case 'o':
-				break;*/
+			}
+				/*case 'e':
+					break;*/
+				case 'g': {
+					fstream file;
+					file.open("refeicao---------------------.txt", ios::out);
+					file << novaref->entrada << endl << novaref->principal << endl << novaref->custo << endl;
+					file.close();
+					/*file.open("infoMesa---------------------.txt", ios::out);
+					while (mesas != NULL) {
+						file << mesas->numMesa << endl << mesas->tamanho << endl << mesas->vagas;
+						while(mesas->pessoas != NULL) {
+							file << mesas->pessoas << endl;
+							mesas->pessoas = mesas->pessoas->seguinte;
+						}
+						mesas = mesas->seguinte;
+					}
+					file.close();*/
+					file.open("infoFilaEspera---------------.txt", ios::out);
+					while (filadeespera != NULL) {
+						if (filadeespera->tipo) {
+							file << filadeespera->tipo << endl << filadeespera->primeironome << endl 
+								<< filadeespera->ultimonome << endl
+								<< filadeespera->numeroid << endl << filadeespera->curso << endl
+								<< filadeespera->plafond << endl << filadeespera->numerogrupo << endl
+								<< filadeespera->duracao << endl << filadeespera->n_elementos << endl;
+							filadeespera = filadeespera->seguinte;
+						}
+						else {
+							file << filadeespera->tipo << endl << filadeespera->primeironome << endl
+								<< filadeespera->ultimonome << endl << filadeespera->numeroid << endl
+								<< filadeespera->plafond << endl << filadeespera->numerogrupo << endl
+								<< filadeespera->duracao << endl << filadeespera->n_elementos << endl;
+							filadeespera = filadeespera->seguinte;
+						}
+					}
+					file.close();
+					cout << "******** Tudo Gravado" << endl;
+					break;
+				}
+				/*case 'c':
+					break;
+				case 'o':
+					break;*/
 
 			}
 		}
@@ -79,6 +116,6 @@ int main() {
 
 	system("PAUSE");
 	return 0;
-		
-	
+
+
 }
